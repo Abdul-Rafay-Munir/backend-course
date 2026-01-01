6f2b2130d091cebfbe745843af596616b1221c43
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-01-01 16:49:53 +0500

    create user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
@@ -12,6 +13,12 @@ const startServer = async () => {
       console.log("ERROR", error);
       throw error;
     });
-    app.listen(process.env.PORT);
-  } catch (error) {}
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("MongoDB connection failed!", err);
+  }
 };
+
+startServer();
